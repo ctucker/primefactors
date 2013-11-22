a569ef00e59591337a0e8ad3cc01fb5cd7c13320
Chris Tucker <chris.tucker@service-now.com>
2013-11-21 20:56:08 -0800

    Initial revision of primefactors

---
+++ src/test/java/Canary.java
@@ -0,0 +1,14 @@
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Canary {
+
+       @Test
+       public void canary() {
+               assertThat(true, is(equalTo(true)));
+       }
+
+}
